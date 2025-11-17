7a6f63e8e560582e136366bcae1cfd70082bedf4
Rakesh8086 <rakeshteja7007@gmail.com>
2025-11-17 09:42:05 +0530

    Initialized spring boot project folder

---
+++ src/test/java/com/flight/bookingapp/FlightBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flight.bookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
